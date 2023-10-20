87c9b63a198289245f7403e094e90b4b2035130d
Andrew Fox <69943907+afox4@users.noreply.github.com>
2023-10-20 13:59:04 -0400

    Update Character.java

--- unit5/Character.java
+++ unit5/Character.java
@@ -1,1 +1,52 @@
+public class Character {
+    
+    private String name;
+    private String typeChar; // is this character a royal, peasant, warrior, troll, wizard, dragon, fairy, or animal
+    private int health;
+    
+    public Character(String initName, String initTypeChar, int initHealth)
+    {
+        name = initName;
+        typeChar = initTypeChar;
+        health = initHealth;
+    }
+    
+    // Getters
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public String getTypeChar()
+    {
+        return typeChar;
+    }
+    
+    public int getHealth()
+    {
+        return health;
+    }
+    
+    // Setters
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
+    
+    
+    // toString()
 
+    public String toString()
+    {
+        return name + " is a member of the character class and is a(n) " + typeChar +".";
+    }
+    
+    public static void main(String args[]) {
+      Character nicholas = new Character("Nicholas", "Warrior", 100);
+      //nicholas.setName("Nick");
+      //System.out.println(nicholas.getName());
+      System.out.println(nicholas);
+      
+    }
+}
